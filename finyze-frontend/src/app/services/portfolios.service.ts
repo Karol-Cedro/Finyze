a43f2fad92299e2c71c03a029719e2d97dc27acf
Karol Cedro <karol-cedro@wp.pl>
2025-10-21 22:56:14 +0200

    add: CORS configuration

--- finyze-frontend/src/app/services/portfolios.service.ts
+++ finyze-frontend/src/app/services/portfolios.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 export interface PortfolioListItem {
   portfolioId: number;
@@ -11,11 +12,16 @@ export interface PortfolioListItem {
   providedIn: 'root'
 })
 export class PortfoliosService {
-  private apiUrl = 'http://localhost:8080/api/users';
+  private apiUrl = `${environment.apiUrl}/users`;
 
   constructor(private http: HttpClient) { }
 
   getPortfolio(userId: number): Observable<PortfolioListItem[]> {
-    return this.http.get<PortfolioListItem[]>(`${this.apiUrl}/${userId}/portfolios`);
+    return this.http.get<PortfolioListItem[]>(`${this.apiUrl}/${userId}/portfolios`).pipe(
+      catchError(error => {
+        console.error('Portfolio service error:', error);
+        return throwError(() => error);
+      })
+    );
   }
 }
